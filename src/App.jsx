5737409cdf6ab431c8976100fcff3bf79628ae23
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-07-18 11:24:55 +0100

    mark: added app's markup
    - added the jsx markup for the app
    - preparing the app for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
+import { BsX } from 'react-icons/bs'
+
 function App() {
   return (
     <div className='app'>
+      <h1 className="app-heading">my shopping list</h1>
+
+      {/* shopping list ctn */}
+      <div className="list-ctn">
+
+        {/* list item */}
+        <div className="list-item">
+          <div>
+            {/* insert delete/cancel icon*/} 
+            <BsX className='delete-icon'/>
+
+            <span className="item-name">
+              golden morn
+            </span>
+          </div>
+
+          <span className="item-price">
+            $20
+          </span>
+        </div>
+
+        {/* list item */}
+        <div className="list-item">
+          <div>
+            {/* insert delete/cancel icon*/} 
+            <BsX className='delete-icon'/>
+
+            <span className="item-name">
+              oreos
+            </span>
+          </div>
+
+          <span className="item-price">
+            $200
+          </span>
+        </div>
+
+        {/* list item */}
+        <div className="list-item">
+          <div>
+            {/* insert delete/cancel icon*/} 
+            <BsX className='delete-icon'/>
+
+            <span className="item-name">
+              iPhone XR
+            </span>
+          </div>
+
+          <span className="item-price">
+            $500
+          </span>
+        </div>
+      </div>
       
+      {/* add item button */}
+      <button className="add-item-btn">
+        Add Item
+      </button>
     </div>
   )
 }
