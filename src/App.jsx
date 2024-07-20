0756858b3313d048bc6fe3995647dfce7fc4519e
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-07-20 04:51:36 +0100

    mark: added app's scripting and functionality
    - added the app's functionality( adding items, removing items )
    - added custom localstorage hook to help abstract localStroage
      complexities while combining the power of useState
    - updated index.html to display correct page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,65 @@
 // child components import
 import ListItem from './ListItem';
 
+// useLocalStorage custom hook import
+import useLocalStorage from './useLocalStorage';
+
 function App() {
-  
+  // create stateful localStorage listItems data
+  const [listItems, setListItems] = useLocalStorage( "react-shopping-list", [] );
+
+  // addItem()
+  // used to add new items to the shopping list
+  function addItem() {
+    // prompt user to enter the new item name
+    var itemName = prompt("Enter an item:");
+
+    // check if new item name is valid else exit the 
+    // function
+    if ( itemName == "" ) {
+      alert("Enter an item to add it to the list");
+      return;
+    }
+
+    // prompt user to enter new item price
+    var itemPrice = prompt("Enter an item's price:");
+    
+    // check if new item price is a valid number else exit the 
+    // function
+    if ( itemPrice == "" || Number.isNaN( itemPrice ) ) {
+      alert("Enter a valid item price to add it to the list");
+      return;
+    }
+
+    // generate new item ID based on listItems length
+    var itemID = listItems.length + 1;
+
+    // update listItems with newly created item
+    setListItems( ( newListItems ) => {
+      newListItems.push(
+        {
+          id: itemID,
+          name: itemName,
+          price: Number( itemPrice ) 
+        }
+      )
+
+      return newListItems
+    } );
+  }
+
+  // deleteItem()
+  // used to delete an item from the shopping list.
+  // accepts an itemID ( to delete ) as an argument
+  function deleteItem( id ) {
+    // remove item with the specified id from listItems
+    var newListItems = listItems.filter( function( item ) {
+      return item.id != id
+    })
+
+    // update listItems with new list
+    setListItems( newListItems );
+  }
 
   return (
     <div className='app'>
@@ -11,18 +68,25 @@ function App() {
       {/* shopping list ctn */}
       <div className="list-ctn">
 
-        {/* list item */}
-        <ListItem name='golden morn' price={200} handleDelete={()=>{}}/>
-
-        {/* list item */}
-        <ListItem name='coffee pill' price={5} handleDelete={()=>{}}/>
-
-        {/* list item */}
-        <ListItem name='iPhone XR' price={400} handleDelete={()=>{}}/>
+        {/* list items */}
+        {
+          ( listItems.length != 0 ) && listItems.map( function( item ) {
+            return <ListItem name={ item.name } price={ item.price } 
+                      handleDelete={() => { deleteItem( item.id ) }} key={item.id}/>
+          })
+        }
+        
+        {/* conditionally render a message if there are no items in listItems */}
+        {
+          ( listItems.length == 0 ) && 
+            <h2 style={ { color: 'lightgray', textAlign: 'center' } }>
+              Add an item to get started
+            </h2>
+        }
       </div>
       
       {/* add item button */}
-      <button className="add-item-btn">
+      <button className="add-item-btn" onClick={addItem}>
         Add Item
       </button>
     </div>
